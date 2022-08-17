40cee6d5f4451cd4dff7bfd4915fcc97c16ade0b
Hind <hind.nayhi@gmail.com>
2022-08-16 20:27:15 -0600

    Implement createTweetElement function to generate the DOM structure for a tweet.

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,3 +4,48 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
+const tweetData = {
+  "user": {
+    "name": "Newton",
+    "avatars": "https://i.imgur.com/73hZDYK.png",
+      "handle": "@SirIsaac"
+    },
+  "content": {
+      "text": "If I have seen further it is by standing on the shoulders of giants"
+    },
+  "created_at": 1461116232227
+};
+
+$(document).ready(() => {
+  $('#tweets-container').append($tweet);
+});
+
+const createTweetElement = function(tweet) {
+  const user = tweet.user;
+  const content = tweet.content;
+  const htmlStructure = `
+  <article class="tweet">
+  <header>
+    <span class="avatar"><img src=${user.avatars}>
+    <div class="name">&nbsp ${user.name}</div></span>
+    <div class="username">${user.handle}</div>
+  </header>
+  <div class="tweet-content">
+    <p class="tweet-content-text">${content.text}</p>
+  </div>
+  <footer>
+    <div class="time">${tweet.created_at}</div>
+    <div class="icons">
+      <span><i class="fa-solid fa-flag"></i>&nbsp</span>
+      <span><i class="fa-solid fa-retweet"></i>&nbsp</span>
+      <span><i class="fa-solid fa-heart"></i></span>
+    </div>
+  </footer>
+  </article>  
+  `;
+
+  return htmlStructure;
+};
+
+const $tweet = createTweetElement(tweetData);
+console.log($tweet);
