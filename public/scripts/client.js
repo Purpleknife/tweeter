a154c991fc754fd488009c426b7821326add1177
Hind <hind.nayhi@gmail.com>
2022-08-17 13:55:17 -0600

    Implement loadTweets function that receives an array of tweets as JSON and renders them to the DOM.

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,37 +4,24 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
-const data = [
-  {
-    "user": {
-      "name": "Newton",
-      "avatars": "https://i.imgur.com/73hZDYK.png"
-      ,
-      "handle": "@SirIsaac"
-    },
-    "content": {
-      "text": "If I have seen further it is by standing on the shoulders of giants."
-    },
-    "created_at": 1461116232227
-  },
-  {
-    "user": {
-      "name": "Descartes",
-      "avatars": "https://i.imgur.com/nlhLi3I.png",
-      "handle": "@rd" },
-    "content": {
-      "text": "Je pense, donc je suis."
-    },
-    "created_at": 1461113959088
-  }
-];
 
 $(document).ready(() => { //Call the callbacks when the document is fully loaded/ ready.
-  renderTweets(data);
   formSubmission();
+  loadTweets();
 });
 
-const formSubmission = function() { //Created to host the event handler and the Ajax request.
+const loadTweets = function() { //Receives an array of tweets as JSON and renders it to the DOM.
+  $.ajax('/tweets', {
+    method: 'GET',
+  })
+  .done(function(response) {
+    renderTweets(response);
+  })
+};
+
+
+
+const formSubmission = function() { //Created to host the event handler and the Ajax POST request.
   const $form = $('#tweet-form');
   $form.submit(function(event) {
     event.preventDefault();
@@ -44,12 +31,12 @@ const formSubmission = function() { //Created to host the event handler and the 
       method: 'POST',
       data: $form.serialize(),
     })
-      .done(function() {
-        console.log('Tweet submitted.');
-      })
-      .fail(function() {
-        console.log('Submission failed.');
-      });
+    .done(function() {
+      console.log('Tweet submitted.');
+    })
+    .fail(function() {
+      console.log('Submission failed.');
+    });
   $('#tweet-text').val(''); //To clear the form after submission.
   $('.counter').val(140); //To reset the counter to its default/ max val: 140.
   });
